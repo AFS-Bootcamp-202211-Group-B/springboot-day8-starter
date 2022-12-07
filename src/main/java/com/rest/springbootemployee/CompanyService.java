9d308f7da849ca8762e45304dd9c4acc3a83d98d
Antony Choi <antony.choi@oocl.com>
2022-12-08 00:12:41 +0800

    feat: implement company service to update company name only

--- src/main/java/com/rest/springbootemployee/CompanyService.java
+++ src/main/java/com/rest/springbootemployee/CompanyService.java
@@ -26,4 +26,12 @@ public class CompanyService {
     public Company create(Company company) {
         return companyRepository.create(company);
     }
+    public Company update(Integer id, Company company) {
+        Company existingCompany = companyRepository.findById(id);
+        if (company.getName() != null) {
+            existingCompany.setName(company.getName());
+        }
+        return existingCompany;
+    }
+
 }

023b2fe10ce1861415a93b0d40088669db7e090a
lijo15 <lijo15@oocl.com>
2022-12-07 13:41:01 +0800

    test: should_get_employees_by_page_when_perform_get_given_employees

--- src/test/java/com/rest/springbootemployee/EmployeeControllerTest.java
+++ src/test/java/com/rest/springbootemployee/EmployeeControllerTest.java
@@ -102,5 +102,25 @@ public class EmployeeControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(10000));
 
     }
+    @Test
+    void should_get_employees_by_page_when_perform_get_given_employees() throws Exception {
+        // given
+        employeeRepository.create(new Employee(11,"one",21,"male",11000));
+        employeeRepository.create(new Employee(10,"two",22,"female",10000));
+        employeeRepository.create(new Employee(12,"three",21,"male",11000));
+        employeeRepository.create(new Employee(12,"four",21,"male",11000));
+
+        // when & then
+        client.perform(MockMvcRequestBuilders.get("/employees?page={page}&pageSize={pageSize}",2,2))
+                // 1. assert response status
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                // 2. assert response data
+                .andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("three"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(21))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].gender").value("male"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].salary").value(11000));
+
+    }
 
 }

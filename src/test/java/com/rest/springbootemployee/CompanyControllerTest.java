1330ed85b1adf15579ce8d578bf51277d91a8880
tokaho <tokaho2000@gmail.com>
2022-12-07 19:15:49 +0800

    [Test] should_get_all_companies_when_perform_get_given_companies

--- src/test/java/com/rest/springbootemployee/CompanyControllerTest.java
+++ src/test/java/com/rest/springbootemployee/CompanyControllerTest.java
@@ -1,10 +1,19 @@
 package com.rest.springbootemployee;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,5 +28,21 @@ public class CompanyControllerTest {
     @BeforeEach
     void cleanRepository(){
         companyRepository.clearAll();
+    }
+
+    @Test
+    void should_get_all_companies_when_perform_get_given_companies() throws Exception{
+        //given
+        companyRepository.create(new Company(100, "spring", new ArrayList<>()));
+
+        //when
+        client.perform(MockMvcRequestBuilders.get("/companies"))
+                //Then
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isNumber())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("spring"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].employees",hasSize(0)));
+
     }
 }

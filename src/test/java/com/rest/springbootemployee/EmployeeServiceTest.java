1dcfab4e1a03ace2119f0fea1d3da0f67a7e6808
Antony Choi <antony.choi@oocl.com>
2022-12-07 16:40:57 +0800

    test: should_update_only_age_and_salary_when_update_all_given_employees

--- src/test/java/com/rest/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/rest/springbootemployee/EmployeeServiceTest.java
@@ -35,12 +35,30 @@ class EmployeeServiceTest {
         //when
         List<Employee> result = employeeService.findAll();
         //then
-        //1. verufy data
+        //1. verify data
         assertThat(result, hasSize(1));
         assertThat(result.get(0), equalTo(employee));
         assertEquals(employees, result);
         //2.verify interaction
         verify(employeeRepository).findAll(); //spy
     }
+    @Test
+    void should_update_only_age_and_salary_when_update_all_given_employees() {
+        //given
+        int employeeId = 1;
+        Employee employee = new Employee(employeeId, "Susan", 22, "Female", 100000);
+        Employee toUpdateEmployee = new Employee(employeeId, "Tom", 23, "Male", 12000);
+        //when
+        when(employeeRepository.findById(employeeId)).thenReturn(employee);
+        Employee updatedEmployee = employeeService.update(employeeId, toUpdateEmployee);
+        //then
+        verify(employeeRepository).findById(employeeId); //spy
+        //verify data
+        assertThat(updatedEmployee.getAge(), equalTo(23));
+        assertThat(updatedEmployee.getSalary(), equalTo(12000));
+        assertThat(updatedEmployee.getName(), equalTo("Susan"));
+        assertThat(updatedEmployee.getGender(), equalTo("Female"));
+    }
+
 
 }

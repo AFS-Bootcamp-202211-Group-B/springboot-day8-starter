acf2824b2e7f2afd7059962f96665726ce436cd9
WONGCH9 <wongch9@oocl.com>
2022-12-07 13:45:18 +0800

    test: added test getEmployeeById and test getEmployeeByGender

--- src/test/java/com/rest/springbootemployee/EmployeeControllerTest.java
+++ src/test/java/com/rest/springbootemployee/EmployeeControllerTest.java
@@ -42,4 +42,45 @@ public class EmployeeControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].gender").value("Female"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].salary").value(10000));
     }
+
+    @Test
+    void should_get_employees_by_id_when_perform_get_by_id_given_employees() throws Exception {
+        //given
+        Employee susan = employeeRepository.create(new Employee(10, "Susan", 22, "Female", 10000));
+        employeeRepository.create(new Employee(10, "Bob", 23, "Male", 9000));
+
+        //when
+        client.perform(MockMvcRequestBuilders.get("/employees/{id}", susan.getId()))
+                // 1. assert response status
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                // 2. assert response date
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Susan"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(22))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.gender").value("Female"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(10000));
+
+
+
+        //then
+    }
+
+    @Test
+    void should_get_employees_by_gender_when_perform_get_by_gender_given_employees() throws Exception {
+        //given
+        Employee susan = employeeRepository.create(new Employee(10, "Susan", 22, "Female", 10000));
+        //employeeRepository.create(new Employee(10, "Bob", 23, "Male", 9000));
+
+        //when
+        client.perform(MockMvcRequestBuilders.get("/employees?gender={gender}", "Female"))
+                // 1. assert response status
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                // 2. assert response date
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Susan"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(22))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].gender").value("Female"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].salary").value(10000));
+
+        //then
+    }
+
 }

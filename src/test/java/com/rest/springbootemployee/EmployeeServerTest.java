18f09fa5d90c4b7c5f811b6adb85c7071f77198b
WONGCH9 <wongch9@oocl.com>
2022-12-07 21:20:57 +0800

    test: add test should_return_employee_when_find_by_id_given_employee_id

--- src/test/java/com/rest/springbootemployee/EmployeeServerTest.java
+++ src/test/java/com/rest/springbootemployee/EmployeeServerTest.java
@@ -61,35 +61,41 @@ public class EmployeeServerTest {
         int employeeId = 1;
         Employee employee = new Employee(employeeId, "Susan", 22, "Female", 10000);
        // employees.add(employee);
-
         Employee toUpdateEmployee = new Employee(employeeId, "Tom", 23, "Male", 12000);
-
-
         when(employeeRepository.findById(employeeId)).thenReturn(employee); //stub
 
         //when
         Employee updatedEmployee = employeeService.update(employeeId, toUpdateEmployee);
 
-
         //then
-
         verify(employeeRepository).findById(employeeId);  // spy
         assertThat(updatedEmployee.getAge(),equalTo(23));
         assertThat(updatedEmployee.getSalary(),equalTo(12000));
 //        assertEquals(updatedEmployee.getAge(), 23);
 //        assertEquals(updatedEmployee.getSalary(), 12000);
-
     }
 
-//    @Test
-//    void should_when_given() {
-//        //given
-//
-//        //when
-//
-//        //then
-//
-//    }
+    @Test
+    void should_return_employee_when_find_by_id_given_employee_id() {
+        //given
+        Employee employee = new Employee(10, "Bobby", 25, "Male", 8000);
+
+        when(employeeRepository.findById(employee.getId())).thenReturn(employee); //stub
+
+        //when
+        Employee result = employeeService.findById(employee.getId());
+
+
+        //then
+        // 1. verify data
+        assertThat(result.getName(),equalTo("Bobby"));
+        assertThat(result.getAge(),equalTo(25));
+        assertThat(result.getGender(),equalTo("Male"));
+        assertThat(result.getSalary(),equalTo(8000));
+        // 2. verify interaction
+        verify(employeeRepository).findById(employee.getId());  // spy
+    }
+
 
 
 }

83102aa2ef6c44b660e5f15b5fcab6bca190ed1d
Wu Xueheng <xhwu@thougtworks.com>
2022-12-07 09:01:19 +0800

    init project

---
+++ src/main/java/com/rest/springbootemployee/CompanyController.java
@@ -0,0 +1,53 @@
+package com.rest.springbootemployee;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+    private CompanyRepository companyRepository;
+
+    public CompanyController(CompanyRepository CompanyRepository) {
+        this.companyRepository = CompanyRepository;
+    }
+
+    @GetMapping
+    public List<Company> getAll() {
+        return companyRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Company getById(@PathVariable Integer id) {
+        return companyRepository.findById(id);
+    }
+
+    @GetMapping("/{id}/employees")
+    public List<Employee> getEmployees(@PathVariable Integer id) {
+        return companyRepository.getEmployees(id);
+    }
+
+    @GetMapping(params = {"page", "pageSize"})
+    public List<Company> getByPage(Integer page, Integer pageSize) {
+        return companyRepository.findByPage(page, pageSize);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Company create(@RequestBody Company company) {
+        return companyRepository.create(company);
+    }
+
+    @PutMapping("/{id}")
+    public Company update(@PathVariable Integer id, @RequestBody Company company) {
+        return companyRepository.update(id, company);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCompany(@PathVariable Integer id) {
+        companyRepository.delete(id);
+    }
+}
---
+++ src/main/java/com/rest/springbootemployee/EmployeeController.java
@@ -0,0 +1,54 @@
+package com.rest.springbootemployee;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getById(@PathVariable Integer id) {
+        return employeeRepository.findById(id);
+    }
+
+    @GetMapping(params = {"gender"})
+    public List<Employee> getByGender(@RequestParam String gender) {
+        return employeeRepository.findByGender(gender);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee add(@RequestBody Employee employee) {
+        return employeeRepository.create(employee);
+    }
+    @PutMapping("/{id}")
+    public Employee update(@PathVariable Integer id, @RequestBody Employee employee) {
+        return employeeRepository.update(id, employee);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        employeeRepository.delete(id);
+    }
+
+
+    @GetMapping(params = {"page", "pageSize"})
+    public List<Employee> getByPage(int page, int pageSize) {
+        return employeeRepository.findByPage(page, pageSize);
+    }
+
+}

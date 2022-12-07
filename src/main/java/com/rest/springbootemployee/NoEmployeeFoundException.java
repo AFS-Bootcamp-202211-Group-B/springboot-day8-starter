97c3019a6cf8c66fa807089ec77b584afa093757
tokaho <tokaho2000@gmail.com>
2022-12-07 17:29:55 +0800

    [Feat] add global exception handler

--- src/main/java/com/rest/springbootemployee/NoEmployeeFoundException.java
+++ src/main/java/com/rest/springbootemployee/NoEmployeeFoundException.java
@@ -1,5 +1,6 @@
 package com.rest.springbootemployee;
 
+
 public class NoEmployeeFoundException extends RuntimeException {
     public NoEmployeeFoundException() {
         super("No employee found");
---
+++ src/main/java/com/rest/springbootemployee/ResponseData.java
@@ -0,0 +1,13 @@
+package com.rest.springbootemployee;
+
+public class ResponseData {
+    public ResponseData(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    private String status;
+    private String message;
+
+
+}

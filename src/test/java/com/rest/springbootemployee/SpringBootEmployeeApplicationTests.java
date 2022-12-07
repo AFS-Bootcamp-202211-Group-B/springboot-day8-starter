117e206e1558d3dedeb455a9e1cc4961ba86b4d8
tokaho <tokaho2000@gmail.com>
2022-12-07 14:32:55 +0800

    [Feat] finish all

--- src/test/java/com/rest/springbootemployee/SpringBootEmployeeApplicationTests.java
+++ src/test/java/com/rest/springbootemployee/SpringBootEmployeeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.rest.springbootemployee;
 
+import com.jayway.jsonpath.JsonPath;
 import com.rest.springbootemployee.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -103,15 +105,26 @@ class SpringBootEmployeeApplicationTests {
 				.andExpect(MockMvcResultMatchers.jsonPath("$[1].salary").value(10001));
 	}
 
-	//test create new employee
+	//test post new employee
 	@Test
 	void should_create_new_employee_when_perform_create_given_employees() throws Exception{
 		//given
 		String newUserJsonString="{\"name\":\"Jim\",\"age\":20,\"gender\":\"Male\",\"salary\":55000}";
 		//when
-		client.perform(MockMvcRequestBuilders.post("/employees").content(newUserJsonString).contentType(MediaType.APPLICATION_JSON))
+		MvcResult result = client.perform(MockMvcRequestBuilders.post("/employees").content(newUserJsonString).contentType(MediaType.APPLICATION_JSON))
 				//Then
 				.andExpect(MockMvcResultMatchers.status().isCreated())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Jim"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.age").value(20))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.gender").value("Male"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(55000))
+				.andReturn();
+//
+		int newUserId = JsonPath.read(result.getResponse().getContentAsString(),"$.id");
+		client.perform(MockMvcRequestBuilders.get("/employees/{id}",newUserId))
+				//Then
+				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Jim"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$.age").value(20))
@@ -120,6 +133,50 @@ class SpringBootEmployeeApplicationTests {
 
 	}
 
+	//test put employee
+	@Test
+	void should_update_employee_when_perform_put_given_employees() throws Exception{
+		//given
+		Employee test1=employeeRepository.create(new Employee(10,"test",22,"Female",10000));
+		String newUserJsonString="{\"name\":\"Jim\",\"age\":20,\"gender\":\"Male\",\"salary\":55000}";
+		//when
+		client.perform(MockMvcRequestBuilders.put("/employees/{id}",test1.getId())
+				.content(newUserJsonString).contentType(MediaType.APPLICATION_JSON))
+				//Then
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("test"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.age").value(20))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.gender").value("Female"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(55000));
+		client.perform(MockMvcRequestBuilders.get("/employees/{id}",test1.getId()))
+				//Then
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").isNumber())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("test"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.age").value(20))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.gender").value("Female"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.salary").value(55000));
+
+	}
+
+	//test delete employee
+	@Test
+	void should_delete_employee_by_id_when_perform_delete_given_employees() throws Exception{
+		//given
+		Employee test1=employeeRepository.create(new Employee(10,"test",22,"Female",10000));
+		//when
+		client.perform(MockMvcRequestBuilders.delete("/employees/{id}",test1.getId()))
+				//Then
+				.andExpect(MockMvcResultMatchers.status().isNoContent());
+
+
+		client.perform(MockMvcRequestBuilders.get("/employees"))
+				//Then
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(0)));
+
+	}
 
 
 
